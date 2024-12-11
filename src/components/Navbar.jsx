6ed55e17bab7ffad8cb77021f061cf996bd03a24
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-12-11 10:46:45 +0500

    setup react router dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,17 @@ import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { ImCross } from 'react-icons/im';
 import Image from './Image';
+import { Link } from 'react-router-dom';
 
 
 const NavLinks = [
-    <a href="/">Home</a>,
-    <a href="/">Trending</a>,
-    <a href="/">Most Popular</a>,
-    <a href="/">About</a>,
-    <a href="/">
+    <Link to="/">Home</Link>,
+    <Link to="/">Trending</Link>,
+    <Link to="/">Most Popular</Link>,
+    <Link to="/">About</Link>,
+    <Link to="/">
         <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
-    </a>
+    </Link>
 ]
 
 const Navbar = () => {
