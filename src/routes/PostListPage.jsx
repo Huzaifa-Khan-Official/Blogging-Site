e70c33e811ce1c44df31396cc5821601e88d5142
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-12-14 13:16:03 +0500

    UI work done

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PostList from '../components/PostList'
+import SideMenu from '../components/SideMenu'
 
 const PostListPage = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>PostListPage</div>
+    <div>
+      <h1 className='mt-5 mb-8 text-2xl'>Development Blog</h1>
+
+      <button onClick={() => setOpen(!open)} className='bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden'>{open ? "Close" : "Filter or Search"}</button>
+
+      <div className='flex flex-col-reverse md:flex-row gap-8'>
+        {/* Post Lists */}
+        <div className=''>
+          <PostList />
+        </div>
+
+        {/* Side Menu */}
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
